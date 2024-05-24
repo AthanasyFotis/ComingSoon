27bef146ecdabe55bb81e1759d55b17bf2069037
Athanasy Fotis <athanasyfotis@gmail.com>
2024-05-24 11:46:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('notify-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you! You will be notified when the portfolio launches.');
+});
